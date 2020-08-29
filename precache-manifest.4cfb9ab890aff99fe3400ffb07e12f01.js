030b9f96f503bf0b20c3555e5883b7fe3f705110
akashdas1811 <adas0442@gmail.com>
2020-08-30 01:27:46 +0530

    Updates

---
+++ precache-manifest.4cfb9ab890aff99fe3400ffb07e12f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852a1ea6dd03b7315c68414aa5f9b051",
+    "url": "/Akash_Das-My_Portfolio/index.html"
+  },
+  {
+    "revision": "854b3cda56822faaa226",
+    "url": "/Akash_Das-My_Portfolio/static/css/main.b1ee08c8.chunk.css"
+  },
+  {
+    "revision": "c8e9ce76aa9444a43bf6",
+    "url": "/Akash_Das-My_Portfolio/static/js/2.7d6d49de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Akash_Das-My_Portfolio/static/js/2.7d6d49de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "854b3cda56822faaa226",
+    "url": "/Akash_Das-My_Portfolio/static/js/main.2846f23c.chunk.js"
+  },
+  {
+    "revision": "71ae48ad00069c38a546",
+    "url": "/Akash_Das-My_Portfolio/static/js/runtime-main.341a2534.js"
+  }
+]);
